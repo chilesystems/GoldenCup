2d6942681ae0e022982dc0bbb2e5182270a6f2f9
Gonzalo Bustamante B <mercocha13@hotmail.com>
2019-09-23 10:06:38 -0300

    Agregar archivos de proyecto.

---
+++ GoldenCup/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GoldenCup
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GoldenCup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoldenCup.MvcApplication" Language="C#" %>
